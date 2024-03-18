47c41150f6f74bd6e3d21553f6d74e98b1f35e98
Benny Mosheyev <bennymsv@gmail.com>
2024-03-18 18:15:19 +0200

    get the form values and proccess them so their format will fit the payment API

--- src/checkout/Checkout.tsx
+++ src/checkout/Checkout.tsx
@@ -25,10 +25,18 @@ export function Checkout() {
     { title: "Price After Discount:", description: "100$" },
   ];
   const [orderDetails, setOrderDetails] = useState(MOCKDATA);
+  const originalPrice = parseInt(MOCKDATA[2].description.slice(0, -1));
+  const [paymentDetails, setPaymentDetails] = useState({
+    holder: "",
+    cc: "",
+    exp: "",
+    cvv: "",
+    charge: originalPrice,
+  });
 
   useEffect(() => {
-    const originalPrice = parseInt(MOCKDATA[2].description.slice(0, -1));
     const priceAfterDiscount = originalPrice - discount;
+    setPaymentDetails({ ...paymentDetails, charge: priceAfterDiscount });
 
     setOrderDetails([
       ...orderDetails.slice(0, 3), // Keep the first three items unchanged
@@ -44,6 +52,17 @@ export function Checkout() {
     }
   };
 
+  const handleFormSubmit = () => {
+    console.log("Payment Details:", paymentDetails);
+  };
+
+  const handleInputChange = (event: { target: { name: any; value: any } }) => {
+    const { name, value } = event.target;
+    setPaymentDetails({ ...paymentDetails, [name]: value });
+  };
+
+  //TODO: add form validations
+
   return (
     <Paper shadow="md" radius="lg">
       <div className={classes.wrapper}>
@@ -57,7 +76,6 @@ export function Checkout() {
 
           <OrderSummaryList orderDetails={orderDetails} />
         </div>
-
         <form
           className={classes.form}
           onSubmit={(event) => event.preventDefault()}
@@ -72,12 +90,20 @@ export function Checkout() {
               label="Your name"
               placeholder="English letters only"
               required
+              value={paymentDetails.holder}
+              onChange={handleInputChange}
+              name="holder"
+
+              //{...form.getInputProps("holder")}
             />
             <TextInput
               style={{ marginTop: "15px" }}
               label="Credit card number"
               placeholder="0-9 digits only"
               required
+              value={paymentDetails.cc}
+              onChange={handleInputChange}
+              name="cc"
             />
             <SimpleGrid cols={{ base: 1, sm: 2 }}>
               <TextInput
@@ -85,12 +111,18 @@ export function Checkout() {
                 label="Expiration date"
                 placeholder="MM/YY"
                 required
+                value={paymentDetails.exp}
+                onChange={handleInputChange}
+                name="exp"
               />
               <TextInput
                 style={{ marginTop: "15px" }}
                 label="cvv"
                 placeholder="cvv"
                 required
+                value={paymentDetails.cvv}
+                onChange={handleInputChange}
+                name="cvv"
               />
             </SimpleGrid>
             <div className={classes.controlscpn}>
@@ -112,7 +144,11 @@ export function Checkout() {
             </div>
 
             <Group justify="flex-start" mt="md">
-              <Button type="submit" className={classes.control}>
+              <Button
+                type="submit"
+                className={classes.control}
+                onClick={handleFormSubmit}
+              >
                 Buy Now
               </Button>
             </Group>
