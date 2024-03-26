e0ae13979625e510d589ce6aba45e611e1ac6d48
galkreiser <galkreiser@gmail.com>
2024-03-26 11:11:21 +0200

    reintalled everything

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "@mantine/core/styles.css";
 import React from "react";
 import { useState } from "react";
 // import { EventForm } from "./components/eventform/eventform";
-// import { Catalog } from "./components/catalog/catalog";
+import { Catalog } from "./components/catalog/catalog";
 // import { UserSpace } from "./components/userspace/UserSpace";
 // import { SuccessPage } from "./success_page/SuccessPage";
 // import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -47,7 +47,7 @@ function App() {
   return (
     <sessionContext.Provider value={providerValues}>
       {/* <RouterProvider router={router}> */}
-      {/* <EventPage /> */}
+      <Catalog />
     </sessionContext.Provider>
   );
 }
