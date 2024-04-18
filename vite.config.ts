54e049341b1adfea1361fb201b4139bb30fd93c9
Benny Mosheyev <bennymsv@gmail.com>
2024-04-18 13:36:40 +0300

    Update package name and base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/events-system-frontend/",
+  base: "/events-system/",
 });
